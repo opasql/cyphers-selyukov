8fb4602093772a9594810b10fb773c5abce34ef2
Oleg Paskal <o.paskal@tinkoff.ru>
2022-12-18 05:49:58 +0400

    Add main loop

---
+++ main.py
@@ -0,0 +1,39 @@
+import caesar
+import homophonic
+
+
+encrypted_messages = []
+
+
+def print_encrypted_messages():
+    print('Encrypted messages: ')
+    for i in range(len(encrypted_messages)):
+        print(f'{i}: {encrypted_messages[i]}')
+
+
+def main_loop():
+    while True:
+        offset = 0
+        method = int(input('1: Шифр Цезаря\n2: Омофоническая замена\n> '))
+        if method == 1:
+            offset = int(input('Введите сдвиг для Шифра Цезаря\n> '))
+        action = int(input('1: Зашифровать\n2: Расшифровать\n> '))
+        message = input('Введите сообщение\n> ')
+        result = ''
+
+        if action == 1:
+            if method == 1:
+                result = caesar.encrypt(message, offset)
+            elif method == 2:
+                result = homophonic.encrypt(message)
+        elif action == 2:
+            if method == 1:
+                result = caesar.decrypt(message, offset)
+            elif method == 2:
+                result = homophonic.decrypt(message)
+
+        print(f'Результат:\n{result}')
+        print('<================>')
+
+
+main_loop()
