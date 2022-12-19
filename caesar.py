f88e58088e604105410f662726a22ef1a0e76e0d
Oleg Pascal <rtrn.0@ya.ru>
2022-12-20 01:28:11 +0400

    Ceasar encryption now works with entire Unicode table

--- caesar.py
+++ caesar.py
@@ -1,16 +1,8 @@
 def encrypt(message, offset):
     result = ""
 
-    # transverse the plain txt
     for i in range(len(message)):
-        char = message[i]
-
-        if (char.isupper()):
-            char = chr((ord(char) - ord('A') + offset) % 26 + ord('A'))
-        elif (char.islower()):
-            char = chr((ord(char) - ord('a') + offset) % 26 + ord('a'))
-
-        result += char
+        result += chr((ord(message[i]) + offset))
     return result
 
 
